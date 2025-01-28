e0c12c3e91559bfe3b10c9356e89ef4bc1cbd001
Reddonut-cyber <akhowsuk@gmail.com>
2025-01-28 13:55:22 +0700

    Add HTML template for the home page and update route to render it

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("index.html")
 
 @app.route("/test/<int:value1>/<int:value2>")
 def test(value1, value2):
     return f"<p>Answer is: {value1**2+value2**2}</p>"
+
 
